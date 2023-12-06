36a889f6cf2be27209ee53193d9d204043c0b578
Jean Lopez <jeanlopezcortes@gmail.com>
2023-12-06 19:52:52 -0300

    getProductById, empty id fix, url changued to 0.0.0.0:50052

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { AddProductRequest, AddProductResponse, DeleteProductByIdRequest, Empty, GetAllProductsResponse, UpdateProductRequest, getProductByIdRequest } from './product.pb';
+import { AddProductRequest, AddProductResponse, DeleteProductByIdRequest, Empty, GetAllProductsResponse, UpdateProductRequest, getProductByIdRequest, getProductByIdResponse } from './product.pb';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Product, ProductDocument } from 'src/mongoose/product.schema';
 
 @Injectable()
@@ -14,6 +14,12 @@ export class ProductService {
 
     async addProduct(request: AddProductRequest): Promise<AddProductResponse> {
         try {
+            const product = await this.productModel.findOne({ name: request.name }).exec();
+            console.log(product);
+            if (product !== null) {
+                return { product: undefined, error: { message: "Product already exists" } }
+            };
+
             const newProduct: Product = {
                 name: request.name,
                 description: request.description,
@@ -23,7 +29,12 @@ export class ProductService {
             const createdProduct = new this.productModel(newProduct);
             await createdProduct.save();
             return {
-                product: createdProduct,
+                product: {
+                    name: createdProduct.name,
+                    description: createdProduct.description,
+                    price: createdProduct.price,
+                    id: createdProduct._id
+                },
                 error: undefined,
             };
         } catch (error) {
@@ -34,19 +45,53 @@ export class ProductService {
         }
     }
 
-   
-    async getAllProducts(request: Empty): Promise<GetAllProductsResponse>  {
+
+    async getAllProducts(request: Empty): Promise<GetAllProductsResponse> {
         try {
-            return { products : await this.productModel.find().exec(), error : undefined};
+            const productList = await this.productModel.find().lean().exec();
+
+            // Transforma cada documento en productList
+            const transformedProducts = productList.map(product => {
+                product.id = product._id.toString();
+                delete product._id;
+                return product;
+            });
+
+            return { products: transformedProducts, error: undefined };
         } catch (error) {
             // Manejo de errores, puedes personalizar según tus necesidades.
             throw new Error(`Error al obtener los productos: ${error.message}`);
         }
     }
 
-    getProductById(request: getProductByIdRequest): import("./product.pb").getProductByIdResponse | PromiseLike<import("./product.pb").getProductByIdResponse> {
-        throw new Error('Method not implemented.');
+
+    async getProductById(request: getProductByIdRequest): Promise<getProductByIdResponse> {
+        try {
+            let objectId;
+            try {
+                objectId = new Types.ObjectId(request.id);
+            } catch (error) {
+                // Si hay un error al convertir, es probable que el id no sea válido
+                console.error(`Error al convertir el id a ObjectId: ${error.message}`);
+                return { product: undefined, error: { message: "Product not Found" } };
+            }
+
+            const productFound = await this.productModel.findOne({ _id: request.id }).exec();
+            
+                const p = {
+                    name: productFound.name,
+                    description: productFound.description,
+                    price: productFound.price,
+                    id: productFound._id
+                };
+                return { product: p, error: undefined };
+
+        } catch (error) {
+            // Manejo de errores, puedes personalizar según tus necesidades.
+            throw new Error(`Error al obtener los productos: ${error.message}`);
+        }
     }
+
 
     deleteProductById(request: DeleteProductByIdRequest): import("./product.pb").DeleteProductByIdResponse | PromiseLike<import("./product.pb").DeleteProductByIdResponse> {
         throw new Error('Method not implemented.');
